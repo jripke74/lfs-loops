5f3a2e8d4066d982524ceae15c7cec87186937b8
jripke74 <jripke74@gmail.com>
2025-11-14 11:51:44 -0800

    add for of loop

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ for (let i = 0; i < cars.length; i++) {
 
 const firstName = "Jeff";
 let position = 1;
-for (letter of firstName) {
+for (let letter of firstName) {
   console.log(`${position++}: ${letter}`);
+}
+for (let carManfacture of cars) {
+  console.log(carManfacture);
+}
+
+const brothers = [
+  { name: "Jason", age: 58 },
+  { name: "Ryan", age: 45},
+  { name: "David", age: 48},
+  { name: "Matthew", age: 38}
+];
+for (let bro of brothers) {
+  console.log(`${bro.name} - ${bro.age}`);
 }
